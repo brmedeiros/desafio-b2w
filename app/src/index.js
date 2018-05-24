a2fbd51b43c773ec363c861564457d722236579a
brmedeiros <br.medeiros@gmail.com>
2018-05-23 22:14:12 -0300

    create-react-app and gitignore setup

---
+++ app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
+import {unregister} from './registerServiceWorker.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+//registerServiceWorker();
+unregister();
